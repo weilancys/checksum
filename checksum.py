82a396233657a3d25e64f393317308425763ece1
LightBlue <weilancys@hotmail.com>
2018-06-27 15:58:44 +0800

    Add files via upload

---
+++ checksum.py
@@ -0,0 +1,37 @@
+import hashlib
+import sys
+import os
+
+usageInfo = "usage: checksum md5|sha256 filename."
+
+if len(sys.argv) != 3:
+    print(usageInfo)
+    sys.exit()
+
+methodName = sys.argv[1]
+fileName = sys.argv[2]
+
+if methodName not in ['md5', 'sha256']:
+    print(usageInfo)
+    sys.exit()
+
+if not os.path.exists(fileName):
+    print("invalid filename.")
+    sys.exit()
+
+if methodName == 'md5':
+    parser = hashlib.md5()
+else:
+    parser = hashlib.sha256()
+
+try:
+    with open(fileName, 'rb') as f:
+        while True:
+            data = f.read(1024)
+            if not data:
+                break
+            parser.update(data)
+except:
+    print("checksum failure")
+
+print(parser.hexdigest())
